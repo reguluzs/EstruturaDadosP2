38080b79189d2446fdb41cc6a2fec6fde8afe20a
Guilherme Moura <guilhermemouraholanda@gmail.com>
2026-06-07 15:37:43 -0300

    Classe Usuario

---
+++ Usuario.java
@@ -0,0 +1,37 @@
+public class Usuario {
+    private int matricula;
+    private String nome;
+    private String email;
+
+    public Usuario(int matricula, String nome, String email) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public int getMatricula(){
+        return matricula;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail (String email) {
+        this.email = email;
+    }
+    @Override
+    public String toString() {
+        return String.format("[%d] %s <%s>", matricula, nome, email);
+    }
+
+    @Override
+    public boolean equals (Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Usuario usuario = (Usuario) obj;
+        return matricula == usuario.matricula;
+    }
+}
